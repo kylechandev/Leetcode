93c341e7173da1d518862b14893a14ec95d01c6e
kylechandev <kylechan47@gmail.com>
2022-11-12 18:21:02 +0800

    add: 分红包问题

--- 小灰/算法的实际应用/AStar.java
+++ 小灰/算法的实际应用/AStar.java
@@ -2,13 +2,16 @@
  * @Author: kaic
  * @Date: 2022-11-12 17:03:39
  * @LastEditors: kylechandev kylechan47@gmail.com
- * @LastEditTime: 2022-11-12 17:05:15
+ * @LastEditTime: 2022-11-12 17:11:56
  * Copyright (c) 2022 by kylechandev kylechan47@gmail.com, All Rights Reserved. 
  */
 package 小灰.算法的实际应用;
 
 /**
- * A星巡路算法
+ * A星巡路算法 - A*search algorithm
+ * 一种用于寻找有效路径的算法
+ * 
+ * 迷宫巡路问题
  */
 public class AStar {
 
---
+++ 小灰/算法的实际应用/Redpackage.java
@@ -0,0 +1,62 @@
+/*
+ * @Author: kaic
+ * @Date: 2022-11-12 18:02:27
+ * @LastEditors: kylechandev kylechan47@gmail.com
+ * @LastEditTime: 2022-11-12 18:20:54
+ * Copyright (c) 2022 by kylechandev kylechan47@gmail.com, All Rights Reserved. 
+ */
+package 小灰.算法的实际应用;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * 红包拆分算法
+ */
+public class Redpackage {
+
+    /**
+     * 拆分红包 - 二倍均值法
+     * 
+     * 每次随机金额的上限定为剩余人均金额的2倍
+     * 
+     * @param totalAmount    总金额（以分为单位）
+     * @param totalPeopleNum 总人数
+     */
+    public static List<Integer> divideRedPackage(Integer totalAmount, Integer totalPeopleNum) {
+        List<Integer> result = new ArrayList<>();
+
+        Random random = new Random();
+
+        Integer restAmount = totalAmount;
+        Integer restPeopleNum = totalPeopleNum;
+
+        for (int i = 0; i < totalPeopleNum - 1; i++) { // 随机分出totalPeopleNum-1个红包
+            // [min, max] n=rand.nextInt(max-min+1)+min
+            int amount = random.nextInt(restAmount / restPeopleNum * 2 - 1 + 1) + 1;
+            result.add(amount);
+
+            // 剩余金额和人数递减
+            restAmount -= amount;
+            restPeopleNum--;
+        }
+        // 最后一份剩下的金额
+        result.add(restAmount);
+
+        int total = 0;
+        for (int i = 0; i < result.size(); i++) {
+            total += result.get(i);
+        }
+        System.out.println("红包总额：" + total);
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Integer totalAmount = 100;
+        Integer totalPeopleNum = 10;
+
+        System.out.println("红包：" + divideRedPackage(totalAmount, totalPeopleNum));
+    }
+}
