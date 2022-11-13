c0802ec8f797bfe093d6f323a719aca1d5d7b276
kylechandev <kylechan47@gmail.com>
2022-11-13 22:37:48 +0800

    optimize code

--- leetcode/链表/反转链表.java
+++ leetcode/链表/反转链表.java
@@ -25,16 +25,8 @@ public class 反转链表 {
         int val;
         ListNode next;
 
-        ListNode() {
-        }
-
         ListNode(int val) {
             this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
         }
     }
 
