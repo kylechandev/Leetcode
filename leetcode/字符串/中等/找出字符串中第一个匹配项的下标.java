83881cec11c55f9b75a71a27c6d39e6f97eaa58e
kylechandev <kylechan47@gmail.com>
2022-12-05 21:37:21 +0800

    add: 字符串

--- leetcode/字符串/中等/找出字符串中第一个匹配项的下标.java
+++ leetcode/字符串/中等/找出字符串中第一个匹配项的下标.java
@@ -2,13 +2,13 @@
  * @Author: kaic
  * @Date: 2022-12-05 15:40:42
  * @LastEditors: kylechandev kylechan47@gmail.com
- * @LastEditTime: 2022-12-05 15:42:25
+ * @LastEditTime: 2022-12-05 21:16:36
  * Copyright (c) 2022 by kylechandev kylechan47@gmail.com, All Rights Reserved. 
  */
 package leetcode.字符串.中等;
 
 /**
- * 28. 找出字符串中第一个匹配项的下标 - KMP
+ * 28. 找出字符串中第一个匹配项的下标 - KMP经典应用
  * 
  * 中等
  * 
@@ -16,9 +16,64 @@ package leetcode.字符串.中等;
  */
 public class 找出字符串中第一个匹配项的下标 {
 
+    /**
+     * KMP
+     * 
+     * @param haystack 原始串
+     * @param needle   匹配串
+     */
     public static int strStr(String haystack, String needle) {
+        if (needle.length() == 0) {
+            return 0;
+        }
+
+        int[] next = new int[needle.length()];
+        getNext(next, needle);
+
+        int j = 0; // 指向模式串
+        for (int i = 0; i < haystack.length(); i++) {
+            // 字符不匹配，模式串位置回退到上一个next数组的位置
+            while (j > 0 && needle.charAt(j) != haystack.charAt(i)) {
+                j = next[j - 1];
+            }
+            // 字符匹配，继续向前匹配
+            if (needle.charAt(j) == haystack.charAt(i)) {
+                j++;
+            }
+
+            if (j == needle.length()) { // j已经把模式串匹配完了，说明已经从原始串中找到模式串了
+                // i指向的是原始串的当前位置，因为原始串已经匹配完成模式串了，所以现在i的位置是模式串的起始位置+模式串的长度，
+                // 所以要获取原始串中模式串的起始位置，就用i再减模式串的长度即可
+                return i - needle.length() + 1;
+            }
+        }
 
         return -1;
+    }
+
+    /**
+     * next数组是`模式串分别取[0,j++]时的相等前后缀长度`（j++表示在各个位置为终点字符串）
+     * 
+     * getNext的过程就相当于是把j当作模式串，把i当作匹配串
+     */
+    private static void getNext(int[] next, String patternString) {
+        // j指向前缀末尾，同时表示`最长相等前后缀`
+        int j = 0;
+        next[0] = 0;
+
+        // i指向后缀末尾
+        for (int i = 1; i < next.length; i++) {
+            while (j > 0 && patternString.charAt(j) != patternString.charAt(i)) {
+                // j回退
+                j = next[j - 1];
+            }
+
+            if (patternString.charAt(j) == patternString.charAt(i)) {
+                j++;
+            }
+
+            next[i] = j;
+        }
     }
 
     public static void main(String[] args) {
--- leetcode/字符串/简单/重复的子字符串.java
+++ leetcode/字符串/简单/重复的子字符串.java
@@ -2,7 +2,7 @@
  * @Author: kaic
  * @Date: 2022-12-05 15:43:13
  * @LastEditors: kylechandev kylechan47@gmail.com
- * @LastEditTime: 2022-12-05 15:44:54
+ * @LastEditTime: 2022-12-05 21:36:27
  * Copyright (c) 2022 by kylechandev kylechan47@gmail.com, All Rights Reserved. 
  */
 package leetcode.字符串.简单;
@@ -12,13 +12,77 @@ package leetcode.字符串.简单;
  * 
  * 简单
  * 
+ * 
+ * 给定一个非空的字符串 s ，检查是否可以通过由它的一个子串重复多次构成。
+ * 
+ * 
+ * 例如：
+ * 输入: s = "abab"
+ * 输出: true
+ * 解释: 可由子串 "ab" 重复两次构成。
+ * 
+ * 
+ * 输入: s = "abcabcabcabc"
+ * 输出: true
+ * 解释: 可由子串 "abc" 重复四次构成。 (或子串 "abcabc" 重复两次构成。)
+ * 
+ * 
  * https://leetcode.cn/problems/repeated-substring-pattern/
  */
 public class 重复的子字符串 {
 
+    /**
+     * KMP解法
+     */
     public static boolean repeatedSubstringPattern(String s) {
+        // abab
+        // 0012
+        // abcabcabcabc
+        // 000123123456
 
-        return false;
+        int length = s.length();
+        // 1、计算next数组
+        int[] next = new int[length];
+        getNext(next, s);
+
+        // 12 % (12-6)
+        // 数组长度 % (数组长度 - 最长相等前后缀的长度)
+        if (next[length - 1] > 0 && (length % (length - next[length - 1])) == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private static void getNext(int[] next, String s) {
+        // abcd
+        // 0000
+        int j = 0;
+        next[0] = 0;
+        for (int i = 1; i < next.length; i++) {
+            while (j > 0 && s.charAt(j) != s.charAt(i)) {
+                j = next[j - 1];
+            }
+            if (s.charAt(j) == s.charAt(i)) {
+                j++;
+            }
+            next[i] = j;
+        }
+    }
+
+    /**
+     * 移动匹配
+     */
+    public static boolean repeatedSubstringPattern2(String s) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(s)
+                // 复制一份
+                .append(s)
+                // 首尾去除一个字符，避免搜索到原始的s
+                .deleteCharAt(0)
+                .deleteCharAt(sb.length() - 1);
+
+        return sb.toString().contains(s);
     }
 
     public static void main(String[] args) {
