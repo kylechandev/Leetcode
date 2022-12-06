45635b3366536044d205125d6d49741af77057e7
kylechandev <kylechan47@gmail.com>
2022-12-06 10:35:12 +0800

    demo

--- Test.java
+++ Test.java
@@ -1,12 +1,8 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 /*
  * @Author: kaic
  * @Date: 2022-11-13 09:17:53
  * @LastEditors: kylechandev kylechan47@gmail.com
- * @LastEditTime: 2022-12-03 21:41:16
+ * @LastEditTime: 2022-12-06 10:34:44
  * Copyright (c) 2022 by kylechandev kylechan47@gmail.com, All Rights Reserved. 
  */
 
@@ -15,18 +11,37 @@ import java.util.List;
  */
 public class Test {
 
-    public static void test() {
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        System.out.println(list);
+    public static int removeElement(int[] nums, int val) {
+        int slowIndex = 0;
+        for (int fastIndex = 0; fastIndex < nums.length; fastIndex++) {
+            if (nums[fastIndex] != val) {
+                nums[slowIndex++] = nums[fastIndex];
+            }
+        }
 
-        List<Integer> unmodifiableList = Collections.unmodifiableList(list);
-        System.out.println(unmodifiableList);
-        unmodifiableList.add(3);
-        System.out.println(unmodifiableList);
+        return slowIndex;
+    }
+
+    public static int removeElement2(int[] nums, int val) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            while (left < right && nums[left] != val) {
+                left++;
+            }
+            while (left < right && nums[right] == val) {
+                right--;
+            }
+            if (left < right) {
+                nums[left++] = nums[right--];
+            }
+        }
+
+        return left;
     }
 
     public static void main(String[] args) {
-        test();
+        System.out.println(removeElement2(new int[] { 3, 2, 2, 3 }, 2));
     }
 }
