dea45b4e36a7564ecd9f3108395f8e41676a2388
kylechandev <kylechan47@gmail.com>
2022-12-08 16:40:19 +0800

    optimize

--- Test.java
+++ Test.java
@@ -1,10 +1,8 @@
-import java.util.Arrays;
-
 /*
  * @Author: kaic
  * @Date: 2022-11-13 09:17:53
  * @LastEditors: kylechandev kylechan47@gmail.com
- * @LastEditTime: 2022-12-07 20:45:48
+ * @LastEditTime: 2022-12-08 16:40:06
  * Copyright (c) 2022 by kylechandev kylechan47@gmail.com, All Rights Reserved. 
  */
 
--- leetcode/栈与队列/简单/用栈实现队列.java
+++ leetcode/栈与队列/简单/用栈实现队列.java
@@ -2,7 +2,7 @@
  * @Author: kaic
  * @Date: 2022-12-06 22:09:48
  * @LastEditors: kylechandev kylechan47@gmail.com
- * @LastEditTime: 2022-12-06 22:20:52
+ * @LastEditTime: 2022-12-08 16:40:14
  * Copyright (c) 2022 by kylechandev kylechan47@gmail.com, All Rights Reserved. 
  */
 package leetcode.栈与队列.简单;
@@ -19,7 +19,7 @@ import java.util.Stack;
 public class 用栈实现队列 {
 
     public static void main(String[] args) {
-        MyQueue myQueue = new MyQueue();
+        new MyQueue();
     }
 }
 
